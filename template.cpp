be5391264024a9aca7f1d3a51b046ee4ade6f9f9
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2025-09-14 00:28:26 +0530

    Template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+#define f(s, n) for(int i = s; i < n; i++)
+
+int main() {
+    ios ::sync_with_stdio(false); cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        
+    }
+}
