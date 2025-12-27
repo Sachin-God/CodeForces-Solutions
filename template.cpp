d24765febe0d0c3c81ac013ed011c93f16cf79aa
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2025-12-27 15:38:36 +0530

    updated Template

--- template.cpp
+++ template.cpp
@@ -8,10 +8,29 @@ typedef vector<long long> vll;
 typedef vector<vector<long long>> vvll;
 #define f(s, n) for(int i = s; i < n; i++)
 
+template <typename T>
+void print_vector(const vector<T>& vec) {
+    for (const auto& x : vec) cout << x << ' ';
+    cout << '\n';
+}
+
+template <typename T>
+void print_grid(const vector<vector<T>>& grid) {
+    for (const auto& row : grid) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << '\n';
+    }
+}
+
 int main() {
     ios ::sync_with_stdio(false); cin.tie(nullptr);
     int t; cin >> t;
     while (t--) {
+        int n; cin >> n;
+        vi a(n);
+        f(0, n) cin >> a[i];
         
     }
 }
