fd43edea6b002b846311e2cf474a021930cb189d
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2026-01-10 23:46:05 +0530

    solved 10/1/26

---
+++ Practice/1200 - 1400/2165A. Cyclic Merging.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define FOR(i, s, n) for(int i = s; i < n; i++) // including i is better bcz nested loop will overwrite upper one
+#define ROF(i, n, s) for(int i = n; i >= s; --i)
+#define FIND(set, target) (set.find(target) != set.end())
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+typedef deque<int> dqi;
+typedef deque<ll> dqll;
+typedef deque<pii> dqii;
+typedef deque<string> dqs;
+
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<pii> qii;
+typedef queue<string> qs;
+
+template <typename T> using maxHeap = priority_queue<T>;
+template <typename T> using minHeap = priority_queue<T, vector<T>, greater<T>>;
+template <typename S, typename T> using umap = unordered_map<S, T>;
+template <typename T> using uset = unordered_set<T>;
+
+template <typename T>
+void print_vector(const vector<T>& vec) {
+    for (const auto& x : vec) cout << x << ' ';
+    cout << '\n';
+}
+
+template <typename T>
+void print_grid(const vector<vector<T>>& grid) {
+    for (const auto& row : grid) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << '\n';
+    }
+}
+
+// this is way too greed approach
+void Wrong() {
+    int n; cin >> n;
+    vi a(n);
+    FOR(i, 0, n) cin >> a[i];
+    ll ans = 0;
+    int i = 0, j = n-1, k = i+1;
+    while (i < j) {
+        int fwd = max(a[i], a[k]);
+        int bck = max(a[i], a[j]);
+        if (fwd < bck) {
+            if (a[i] < a[k]) {
+                i = k; k += 1;
+            } else k += 1;
+            ans += fwd;
+        } else {
+            if (a[i] < a[j]) {
+                i = k; k += 1;
+            } else j -= 1;
+            ans += bck;
+        }
+    }
+    cout << ans << "\n";
+}
+
+void Solve() {
+    int n; cin >> n;
+    vi a(n);
+    FOR(i, 0, n) cin >> a[i];
+    vll q;
+    q.pb(max(a[0], a[n-1]));
+    FOR(i, 0, n-1) q.pb(max(a[i], a[i+1]));
+    sort(all(q));
+    ll ans = 0;
+    FOR(i, 0, n-1) ans += q[i];
+    cout << ans << "\n";
+}
+
+int main() {
+    ios ::sync_with_stdio(false); cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        Solve();               
+    }
+}
