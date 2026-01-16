c3613a1d458656a66f081723863771da7995c09f
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2026-01-16 23:32:36 +0530

    SOLVED TODAY 16/1/26

---
+++ Practice/1200 - 1400/2151C. Incremental Stay.cpp
@@ -0,0 +1,88 @@
+// 34 mins
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define FOR(i, s, n) for(int i = s; i < n; i++) // including i is better bcz nested loop will overwrite upper one
+#define ROF(i, n, s) for(int i = n; i >= s; --i)
+#define FIND(set, target) (set.find(target) != set.end())
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+typedef deque<int> dqi;
+typedef deque<ll> dqll;
+typedef deque<pii> dqii;
+typedef deque<string> dqs;
+
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<pii> qii;
+typedef queue<string> qs;
+
+template <typename T> using maxHeap = priority_queue<T>;
+template <typename T> using minHeap = priority_queue<T, vector<T>, greater<T>>;
+template <typename S, typename T> using umap = unordered_map<S, T>;
+template <typename T> using uset = unordered_set<T>;
+
+template <typename T>
+void print_vector(const vector<T>& vec) {
+    for (const auto& x : vec) cout << x << ' ';
+    cout << '\n';
+}
+
+template <typename T>
+void print_grid(const vector<vector<T>>& grid) {
+    for (const auto& row : grid) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << '\n';
+    }
+}
+
+void Solve() {
+    int n; cin >> n;
+    vll data(2*n), diff1 = {0}, diff2 = {0};
+    FOR(i, 0, 2*n) {
+        cin >> data[i];
+        if (i & 1) {
+            diff1.pb(diff1.back() + data[i] - data[i-1]);
+        } else {
+            if (i > 0) diff2.pb(diff2.back() + data[i] - data[i-1]);
+        }
+    }
+
+    int k = 0, l = n, a = 0, b = n-1;
+    ll fix = 0;
+    FOR(i, 0, n) {
+        if (i & 1) {
+            cout << fix + diff2[b--] - diff2[a++] << ' ';
+        } else {
+            cout << fix + diff1[l--] - diff1[k++] << ' ';
+        }
+        fix += data[2*n - i - 1] - data[i];
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios ::sync_with_stdio(false); cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        Solve();               
+    }
+}
