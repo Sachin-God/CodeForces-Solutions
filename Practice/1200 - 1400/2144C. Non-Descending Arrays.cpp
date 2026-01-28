26054d6cfb067f1e52831409bc8c78b9add784c6
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2026-01-28 14:03:50 +0530

    Practice

---
+++ Practice/1200 - 1400/2144C. Non-Descending Arrays.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define FOR(i, s, n) for(int i = s; i < n; i++) // including i is better bcz nested loop will overwrite upper one
+#define ROF(i, n, s) for(int i = n; i >= s; --i)
+#define FIND(set, target) (set.find(target) != set.end())
+#define ent '\n'
+#define LSOne(S) ((S) & -(S))
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<long long> vll;
+typedef vector<vector<long long>> vvll;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+
+typedef deque<int> dqi;
+typedef deque<ll> dqll;
+typedef deque<pii> dqii;
+typedef deque<string> dqs;
+
+typedef queue<int> qi;
+typedef queue<ll> qll;
+typedef queue<pii> qii;
+typedef queue<string> qs;
+
+template <typename T> using maxHeap = priority_queue<T>;
+template <typename T> using minHeap = priority_queue<T, vector<T>, greater<T>>;
+template <typename S, typename T> using umap = unordered_map<S, T>;
+template <typename T> using uset = unordered_set<T>;
+
+template <typename T>
+void print_vector(const vector<T>& vec) {
+    for (const auto& x : vec) cout << x << ' ';
+    cout << '\n';
+}
+
+template <typename T>
+void print_grid(const vector<vector<T>>& grid) {
+    for (const auto& row : grid) {
+        for (const auto& elem : row) {
+            cout << elem << " ";
+        }
+        cout << '\n';
+    }
+}
+
+template <typename T> 
+void print(T x) {cout << x << ent;}
+
+void Solve() {
+    ll mod = 998244353;
+    int n; cin >> n;
+    vi a(n), b(n);
+    FOR(i, 0, n) cin >> a[i];
+    FOR(i, 0, n) cin >> b[i];
+    ll ans = 2;
+    FOR(i, 0, n) {
+        if (a[i] > b[i]) swap(a[i], b[i]);
+        if (i > 0 && a[i] >= b[i-1]) ans = (ans*2) % mod;
+    }
+    print(ans);
+}
+
+int main() {
+    ios ::sync_with_stdio(false); cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) {
+        Solve();               
+    }
+}
