66cc4a543051a2cc5cba892fa627c38be794e429
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2026-02-15 22:47:20 +0530

    updated template

--- .Templates/template.cpp
+++ .Templates/template.cpp
@@ -58,6 +58,34 @@ void print_grid(const vector<vector<T>>& grid) {
 template <typename T> 
 void print(T x) {cout << x << ent;}
 
+class DSU {
+private:
+    vi parent, rnk;
+public:
+    DSU(int n) {
+        parent.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+        rnk.resize(n, 0);
+    }
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void _union(int x, int y) {
+        int px = find(x), py = find(y);
+        int rx = rnk[px], ry = rnk[py];
+        if (px == py) return;
+        if (rx < ry) parent[px] = py;
+        else if (rx > ry) parent[py] = px;
+        else {
+            parent[py] = px;
+            rnk[px]++;
+        }
+    }
+};
+
 void Solve() {
     int n; cin >> n;
 }
